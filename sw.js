9623aa0ac3d9fce9b510b6555d8ee22977cbe808
Chad <chad.claudebot@gmail.com>
2026-05-30 15:58:53 +0800

    fix logged in

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Provides offline caching & install-ability
 // ═══════════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'bk-pos-v6';
+const CACHE_NAME = 'bk-pos-v7';
 
 // Files to cache for full offline use
 const SHELL_ASSETS = [
